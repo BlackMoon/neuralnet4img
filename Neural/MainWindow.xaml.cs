45f1f96f541c2c783a699d9ecd3c17d7e51b04af
rrv <admin@mail.ru>
2014-05-09 23:15:13 +0400

    Slider

--- Neural/MainWindow.xaml.cs
+++ Neural/MainWindow.xaml.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace Neural
 {
-    enum eActions
-    {
-        ActionOpen,
-        ActionExit
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -18,19 +14,30 @@ namespace Neural
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void OpenCommandHandler(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
-
-            // Set filter options and filter index.
-            dlg.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            dlg.Filter = "Точечный рисунок Windows (BMP)|*.bmp|Формат JPEG|*.jpg|Portable Network Graphics (PNG)|*.png|Все файлы|*.*";
             dlg.FilterIndex = 1;
+            
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    System.Windows.Media.ImageSource src = new BitmapImage(new Uri(dlg.FileName));                    
+                    imgOrig.Source = src;
 
-            // Call the ShowDialog method to show the dialog box.
-            dlg.ShowDialog();
+                    lblInfo.Text = dlg.SafeFileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }      
 
         private void ExitItem_Click(object sender, RoutedEventArgs e)

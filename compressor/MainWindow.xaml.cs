944e95c172abb6e1c2c2c7e68778e059f19b65c6
rrv <admin@mail.ru>
2014-05-11 13:10:00 +0400

    bitmapImage

--- compressor/MainWindow.xaml.cs
+++ compressor/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.Win32;
 using System;
 using System.Drawing;
+using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
@@ -14,6 +15,8 @@ namespace compressor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random rand = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -87,6 +90,9 @@ namespace compressor
             
             byte[] pixels = new byte[len];
             bmp.CopyPixels(pixels, stride, 0);
+            
+            // input
+            double[] input = new double[len];
 
             uint [] hexes = new uint[len4];
             for (int i = 0, j = 0; i < len; i+= 4, j++)
@@ -96,10 +102,76 @@ namespace compressor
                 byte r = pixels[i + 2];
                 byte a = pixels[i + 3];
 
-                hexes[j] = (uint)((a << 24) | (r << 16) | (g << 8) | b); 
+                //hexes[j] = (uint)((a << 24) | (r << 16) | (g << 8) | b); 
+                input[i] = (double)pixels[i];
             }
 
-            Network nt = new Network(len4, 3);
+            DistanceNetwork nt = new DistanceNetwork(len4, 100);
+
+            SOMLearning	trainer = new SOMLearning( nt );
+
+			
+
+			double	fixedLearningRate = 0.1/*learningRate*/ / 10;
+			double	driftingLearningRate = fixedLearningRate * 9;
+
+            int iterations = 100;
+			// iterations
+			int k = 0;
+
+			// loop
+            while (true)
+            {
+                trainer.LearningRate = driftingLearningRate * (iterations - k) / iterations + fixedLearningRate;
+                trainer.LearningRadius = (double)15/*radius*/ * (iterations - k) / iterations;
+
+
+
+                /*
+                input[0] = rand.Next(256);
+                input[1] = rand.Next(256);
+                input[2] = rand.Next(256);
+                input[3] = rand.Next(256);*/
+
+                trainer.Run(input);
+
+
+                // increase current iteration
+                k++;
+
+                // set current iteration's info
+                //currentIterationBox.Text = i.ToString( );
+
+                // stop ?
+                if (k >= iterations)
+                    break;
+            }
+
+
+            byte[] array = new byte[100];
+            for (int i = 0; i < nt.Output.Length; i++)
+            {
+                array[i] = (byte)nt.Output[i];
+            }
+            
+            try
+            {   
+                BitmapImage bm = new BitmapImage();                
+                //bm.BeginInit();
+                bm.CacheOption = BitmapCacheOption.OnLoad;
+                bm.StreamSource = new MemoryStream(array);
+                //bm.EndInit();
+
+                imgMap.Height = 100;
+                imgMap.Width = 100;
+                imgMap.Source = bm;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+            
+            
         }
     }
 }
